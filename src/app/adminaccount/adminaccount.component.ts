e24ac4a46b51a3a3ef4f0be78ceaa33ed5c5a957
endu <endu2012@gmail.com>
2025-06-27 15:24:03 +0100

    added admin account

--- src/app/adminaccount/adminaccount.component.ts
+++ src/app/adminaccount/adminaccount.component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RegisteraccountComponent } from '../registeraccount/registeraccount.component';
+import { Account } from '../auth-service.service';
+import { AuthService } from '../auth-service.service';
+
+
 
 @Component({
+  standalone: true,
   selector: 'app-adminaccount',
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule,FormsModule],
   templateUrl: './adminaccount.component.html',
   styleUrl: './adminaccount.component.css'
 })
-export class AdminaccountComponent {
+export class AdminaccountComponent implements OnInit {
+
+  form! : FormGroup;
+  serarchText: string ="";
+  account : Account[]=[];
+
+  constructor( private fb :FormBuilder,private auth: AuthService) {
+    
+  }
+
+  ngOnInit(): void {
+    
+    this.form = this.fb.group({
+      
+      fullName: [],
+      email: [],
+      password: []
+
+ 
+    });
+  this.loadContent();
+  console.log(this.account);
+  }
+
+  getFilteredAccount() :Account[]{
+ return this.account.filter(a=>
+  a.fullName?.toLowerCase().includes(this.serarchText.toLowerCase()) ||
+  a.email?.toLowerCase().includes(this.serarchText.toLowerCase())
+ )
+  }
+  loadContent(){
+    this.auth.getAccount().subscribe({
+      next : (data)=> { 
+        console.log('Loaded accounts:', data); 
+        this.account=data
+       },
+      error :()=> {
+        alert("failed to load data");
+      }
+    })
+  }
+  deleteAccount(id : number) {
+   
+   this.auth.deleteAccount(id).subscribe({
+    next: (data) => {
+      alert("the Account is deleted successfully!")
+        this.loadContent();
+    },
+    error : ()=> { alert("unable to delete the Account")}
+   })
+
+  }
+
 
 }
+
--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -1,10 +1,20 @@
 // auth.service.ts
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+export interface Account {
+  id: number;
+  fullName: string,
+  email  : string,
+}
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor(private router: Router) {}
+   private baseUrl = 'http://localhost:5139/api/auth/get';
+     private baseUrl2 = 'http://localhost:5139/api/auth';
+  constructor(private router: Router, private http: HttpClient ) {}
 
   isLoggedIn(): boolean {
     return !!localStorage.getItem('token');
@@ -13,5 +23,13 @@ export class AuthService {
   logout() {
     localStorage.removeItem('token');
     this.router.navigate(['/login']);
+  }
+  getAccount():Observable<Account[]>{
+    return this.http.get<Account[]>(this.baseUrl)
+  }
+ 
+  deleteAccount(id: number):Observable<void> {
+     const url = `${this.baseUrl2}/${id}`
+    return this.http.delete<void>(url);
   }
 }
